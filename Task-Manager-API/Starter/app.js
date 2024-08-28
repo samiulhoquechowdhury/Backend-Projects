b492ba270efb95c2fbb4f740b26f892331136978
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-08-28 17:56:28 +0530

    first commit

---
+++ Task-Manager-API/Starter/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+
+//routes
+app.get('/hello', (req,res)=>{
+res.send('Task Manager App');
+})
+const port = 3000;
+
+app.listen(port, ()=>{
+     console.log(`Server is running at PORT ${port}..`);
+})
+
+
