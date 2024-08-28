45739653058ffdf38cc53bbb56706cc595aa0318
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-08-28 23:35:46 +0530

    initial cpmmit

--- Task-Manager-API/Starter/app.js
+++ Task-Manager-API/Starter/app.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express();
+const tasks = require('./routes/tasks')
+
+
+//middleware 
+app.use(express.json());
+
 
 //routes
-app.get('/hello', (req,res)=>{
-res.send('Task Manager App');
+app.get('/hello', (req, res) => {
+     res.send('Task Manager App');
 })
-const port = 3000;
 
-app.listen(port, ()=>{
+app.use('/api/v1/tasks', tasks)
+
+
+
+const port = 3000;
+app.listen(port, () => {
      console.log(`Server is running at PORT ${port}..`);
 })
-
-
