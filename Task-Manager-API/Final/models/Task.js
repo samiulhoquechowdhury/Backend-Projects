116036d27a3838e9d22fb321ab959c825be4b4f3
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-08-29 10:13:29 +0530

    mongo connect

--- Task-Manager-API/Final/models/Task.js
+++ Task-Manager-API/Final/models/Task.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'must Provise name'],
+        required: [true, 'must Provide name'],
         trim: true,
         maxLenght: [20, 'name cannot be more than 20 characters'],
     },
