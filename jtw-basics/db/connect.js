deb781b8ccbfcd4ab84353e81a88d8e3b8afac00
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-05 23:38:04 +0530

    db set-up

--- jtw-basics/db/connect.js
+++ jtw-basics/db/connect.js
@@ -1,12 +1,12 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// const connectDB = (url) => {
-//   return mongoose.connect(url, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   })
-// }
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+}
 
-// module.exports = connectDB
+module.exports = connectDB
