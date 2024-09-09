a7f2caa709fdd55cb72e4baa069d21b1b85cf72e
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-09 20:40:59 +0530

    Route

--- Auth/Backend/Routes/AuthRouter.js
+++ Auth/Backend/Routes/AuthRouter.js
@@ -1,11 +1,11 @@
-// const { signup } = require('../Controllers/AuthController');
-// const { signupValidation } = require('../Middlewares/AuthValidation');
+const { signup } = require('../Controllers/AuthController');
+const { signupValidation } = require('../Middlewares/AuthValidation');
 
-// const router = require('express').Router()
+const router = require('express').Router()
 
-// router.post('/login',(req,res) =>{
-//     res.send('Login Successful')
-// })
+router.post('/login',(req,res) =>{
+    res.send('Login Successful')
+})
 
-// router.post('/signup', signupValidation, signup);
+router.post('/signup', signupValidation, signup);
 
