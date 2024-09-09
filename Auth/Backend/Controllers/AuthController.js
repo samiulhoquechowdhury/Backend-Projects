593fcfa49dc8e935bf935618785ac649af448d23
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-09 21:00:59 +0530

    hashing the user password using bcrypt

--- Auth/Backend/Controllers/AuthController.js
+++ Auth/Backend/Controllers/AuthController.js
@@ -1,13 +1,24 @@
+const bcrypt = require('bcrypt');
+const UserModel = require('../Models/User');
 
 
-// const signup = async (req, res) => {
-//     try {
-//         const { name, email, password } = req.body;
-//     } catch (error) {
+const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await UserModel
+        if (user) {
+            return res.status(409)
+                .json({ message: "User already exist, login", success: false })
+        }
+        const userModel = new UserModel({ name, email, password })
+        userModel.password = await bcrypt.hash(password, 10);
+        await userModel.save();
+        
+    } catch (error) {
 
-//     }
-// }
+    }
+}
 
-// module.exports = {
-//     signup
-// }
+module.exports = {
+    signup
+}
