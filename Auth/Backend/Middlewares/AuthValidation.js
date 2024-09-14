3abc4b2d3bd51d29110a1505a640c6d66475bce3
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-14 12:24:26 +0530

    server side validation

--- Auth/Backend/Middlewares/AuthValidation.js
+++ Auth/Backend/Middlewares/AuthValidation.js
@@ -4,7 +4,7 @@ const signupValidation = (req, res, next) => {
     const schema = Joi.object({
         name: Joi.string().min(3).max(100).required(),
         email: Joi.string().email().required(),
-        password: Joi.string().min(4).max(100).required()
+        password: Joi.string().min(6).max(100).required()
     });
     const { error } = schema.validate(req.body);
     if (error) {
